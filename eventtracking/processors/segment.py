cb3517a952520d54d05a5deed8b5773572f2eba6
Bryan Wilson <bryanlandia@gmail.com>
2020-04-29 18:29:43 -0700

    Add a Processor for Segment to push 'data' contents to top level of event dict

---
+++ eventtracking/processors/segment.py
@@ -0,0 +1,28 @@
+"""Process events to be more useful with Segment backend"""
+
+from __future__ import absolute_import
+from eventtracking.processors.exceptions import EventEmissionExit
+
+
+class SegmentTopLevelPropertiesProcessor(object):
+    """
+
+    Most Segment.io Destination APIs require properties to be at the top level
+    of the event. Copy all properties contained within the event's 'data' key
+    to the top level of the event dict.
+
+    We duplicate instead of reassign to not break previous integrations.
+
+    Returns the event
+    """
+
+    def __call__(self, event):
+        try:
+            for key, val in event['data'].items():
+                try:
+                    event[key].update(event['data'][key])
+                except (KeyError, AttributeError):
+                    event[key] = val
+        except KeyError:  # no data
+            pass
+        return event
