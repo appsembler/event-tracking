e8126a204565198d65ffd4cd474c6115680d968f
Bryan Wilson <bryanlandia@gmail.com>
2020-05-01 11:29:53 -0700

    Segment processor better docstring

--- eventtracking/processors/segment.py
+++ eventtracking/processors/segment.py
@@ -8,7 +8,27 @@ class SegmentTopLevelPropertiesProcessor(object):
     of the event. Copy all properties contained within the event's 'data' key
     to the top level of the event dict.
 
-    We duplicate instead of reassign to not break previous integrations.
+    We copy properties instead of reassign to not break previous integrations.
+
+    For example: by default, tracker.emit() will produce an event
+    with most interesting properties in the `data` key, like:
+
+    analytics.track('13', 'edx.bi.user.chapter.started', {
+    'context': { ... },
+    'data': {
+        'block_id': 'block-v1:foo+101+forever+type@chapter+block@3707382f0c284b6aadbd7c50d767ca8f',
+        'block_name': 'Section 1',
+        'completion_percent': 33.3333333333333,
+        'course_id': 'course-v1:foo+101+forever',
+        'course_name': 'CompletionTest',
+        'label': 'chapter Section 1 started'
+    },
+    'name': 'edx.bi.user.chapter.started',
+    ...
+    })
+
+    Many/most Segment Destinations will not be able to access properties inside 'data'.
+    Copy these to the top level of the event before emitting.
 
     Always returns the event for continued processing.
     """
@@ -20,6 +40,6 @@ class SegmentTopLevelPropertiesProcessor(object):
                     event[key].update(event['data'][key])
                 except (KeyError, AttributeError):
                     event[key] = val
-        except KeyError:  # no data
+        except KeyError:  # no 'data'
             pass
         return event
